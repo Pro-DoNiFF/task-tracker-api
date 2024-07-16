7db9fc2f0665d7f73887c0f296edb7d2bb103941
Pro-DoNiFF <88083364+Pro-DoNiFF@users.noreply.github.com>
2024-07-16 19:01:11 +0300

    Exception for the insert not founded

---
+++ src/main/java/com/doniff/tasktrackerapi/store/api/exceptions/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.doniff.tasktrackerapi.store.api.exceptions;
+
+public class NotFoundException extends Exception {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
