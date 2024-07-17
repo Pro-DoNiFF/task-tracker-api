7f2d174b7de0406e6b6f161d2e8e46e6371188cf
Pro-DoNiFF <88083364+Pro-DoNiFF@users.noreply.github.com>
2024-07-17 17:28:45 +0300

    Create the new method

--- src/main/java/com/doniff/tasktrackerapi/store/repositories/TaskStateRepository.java
+++ src/main/java/com/doniff/tasktrackerapi/store/repositories/TaskStateRepository.java
@@ -3,5 +3,8 @@ package com.doniff.tasktrackerapi.store.repositories;
 import com.doniff.tasktrackerapi.store.entities.TaskStateEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TaskStateRepository extends JpaRepository<TaskStateEntity, Long> {
+    Optional<TaskStateEntity> findTaskStateEntityByProjectIdAndNameContainsIgnoreCase(Long projectId, String taskStateName);
 }
